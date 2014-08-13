8dd4d71a76f58f7ce4d4ca37625db38f89b15d5d
Micah Lee <micah@micahflee.com>
2014-08-12 23:17:14 -0400

    wrote test_scfile_load_small_file test

--- test/supercipher_scfile_test.py
+++ test/supercipher_scfile_test.py
@@ -1,22 +1,43 @@
 from nose import with_setup
 from supercipher.scfile import *
+from supercipher import version, get_tmp_dir, destroy_tmp_dir
+
+def setup_tmp_dir():
+    "make a new tmp_dir"
+    global tmp_dir
+    tmp_dir = get_tmp_dir()
+
+def teardown_tmp_dir():
+    "destroy the existing tmp_dir"
+    global tmp_dir
+    destroy_tmp_dir(tmp_dir)
 
 def test_scfile_version_to_bytes():
     "SuperCipherFile.version_to_bytes should convert the version to 3 bytes"
-    scf = SuperCipherFile('1.1.1')
+    scf = SuperCipherFile(version)
     assert scf.version_to_bytes('1.5.9') == '\x01\x05\x09'
     assert scf.version_to_bytes('0.0.3') == '\x00\x00\x03'
     assert scf.version_to_bytes('255.255.255') == '\xff\xff\xff'
 
 def test_scfile_bytes_to_version():
     "SuperCipherFile.bytes_to_version should convert 3 bytes to a version string"
-    scf = SuperCipherFile('1.1.1')
+    scf = SuperCipherFile(version)
     assert scf.bytes_to_version('\x01\x05\x09') == '1.5.9'
     assert scf.bytes_to_version('\x00\x00\x03') == '0.0.3'
     assert scf.bytes_to_version('\xff\xff\xff') == '255.255.255'
 
+@with_setup(setup_tmp_dir, teardown_tmp_dir)
 def test_scfile_load_small_file():
     "SuperCipher files should all be at least 24 bytes"
+    global tmp_dir
+    scf = SuperCipherFile(version)
+
+    try:
+        scf.load(os.path.abspath('test/data/fake_too_small.sc'), tmp_dir)
+    except:
+        assert True
+    else:
+        assert False
 
 def test_scfile_load_wrong_magic_number():
     "SuperCipher files should begin with the magic number 0xEBA34B1C"
