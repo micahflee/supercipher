6d501b932f97673026ab3c7a08d07b2b4faaba01
Micah Lee <micah@micahflee.com>
2014-08-15 12:57:00 -0400

    placeholder tests for supercipher

--- test/supercipher_test.py
+++ test/supercipher_test.py
@@ -1,5 +1,26 @@
 from supercipher import *
 
-def test_stub():
-    "This is the supercipher stub test"
+def test_encrypt_decrypt():
+    "should be able to encrypt a file with a random passphrase and decrypt it again"
     pass
+
+def test_encrypt_decrypt_multiple_files():
+    "should be able to encrypt multiple files with a random passphrase and decrypt it again"
+    pass
+
+def test_encrypt_decrypt_files_dirs():
+    "should be able to encrypt files and directories with a random passphrase and decrypt it again"
+    pass
+
+def test_encrypt_decrypt_pubkey():
+    "should be able to encrypt/decrypt files using random passphrase and a pubkey"
+    pass
+
+def test_encrypt_decrypt_missing_pubkey():
+    "should fail to encrypt when trying to encrypt files using random passphrase and a pubkey that is missing"
+    pass
+
+def test_encrypt_decrypt_missing_seckey():
+    "should fail to decrypt when trying to decrypt files encrypted using random passphrase and a seckey that is missing"
+    pass
+
